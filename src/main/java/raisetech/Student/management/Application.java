09f6e8fe2fc41eecdfe0aa0ee42e991ea90803a0
Kogure59 <137606211+Kogure59@users.noreply.github.com>
2025-07-15 21:37:10 +0900

    Update Application.java

--- src/main/java/raisetech/Student/management/Application.java
+++ src/main/java/raisetech/Student/management/Application.java
@@ -1,6 +1,5 @@
 package raisetech.Student.management;
 
-import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
