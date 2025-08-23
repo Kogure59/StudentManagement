8b82182b98c3b2eeeb28b6a1401d853579252446
Kogure59 <137606211+Kogure59@users.noreply.github.com>
2025-08-23 18:20:31 +0900

    feat: 受講生コース情報登録処理の実装

--- src/main/java/raisetech/Student/management/controller/StudentController.java
+++ src/main/java/raisetech/Student/management/controller/StudentController.java
@@ -1,5 +1,6 @@
 package raisetech.Student.management.controller;
 
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -42,7 +43,9 @@ public class StudentController {
 
   @GetMapping("/newStudent")
   public String newStudent(Model model) {
-    model.addAttribute("studentDetail", new StudentDetail());
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudentsCourses(Arrays.asList(new StudentsCourses()));
+    model.addAttribute("studentDetail", studentDetail);
     return "registerStudent";
   }
 
--- src/main/java/raisetech/Student/management/service/StudentService.java
+++ src/main/java/raisetech/Student/management/service/StudentService.java
@@ -1,5 +1,6 @@
 package raisetech.Student.management.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,5 +32,11 @@ public class StudentService {
   @Transactional
   public void registerStudent(StudentDetail studentDetail) {
     repository.registerStudent(studentDetail.getStudent());
+    for (StudentsCourses studentsCourse : studentDetail.getStudentsCourses()) {
+      studentsCourse.setStudentId(studentDetail.getStudent().getId());
+      studentsCourse.setCourseStartAt(LocalDateTime.now());
+      studentsCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
+      repository.registerStudentsCourses(studentsCourse);
+    }
   }
 }
