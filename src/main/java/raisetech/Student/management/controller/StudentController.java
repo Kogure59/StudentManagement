b79b306e7d0def8f781d89b782246498f3151744
Kogure59 <137606211+Kogure59@users.noreply.github.com>
2025-09-05 21:32:55 +0900

    chore: 入力チェックのセットアップ

--- src/main/java/raisetech/Student/management/controller/StudentController.java
+++ src/main/java/raisetech/Student/management/controller/StudentController.java
@@ -1,8 +1,10 @@
 package raisetech.Student.management.controller;
 
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import raisetech.Student.management.service.StudentService;
 /**
  * 受講生の検索や登録、更新などを行うREST APIとして受け付けるController
  */
+@Validated
 @RestController
 public class StudentController {
 
